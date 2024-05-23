6070ca8de59e126a861975ce0c7b834b380dc7e5
mohamedx2 <hamroun1969@gmail.com>
2024-05-23 15:37:20 +0100

    Update callenderRoute.js

--- routes/callenderRoute.js
+++ routes/callenderRoute.js
@@ -1,2 +1,16 @@
-const router = require("express").Router();
-router.post()
+const express = require('express');
+const calendarController = require('../controllers/calendarController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+// Routes
+router.post('/', authMiddleware, calendarController.createCalendar);
+router.get('/user', authMiddleware, calendarController.getCalendarsByUser);
+router.get('/', authMiddleware, calendarController.getAllCalendars);
+router.get('/:id', authMiddleware, calendarController.getCalendarById);
+router.put('/:id', authMiddleware, calendarController.updateCalendar);
+router.delete('/:id', authMiddleware, calendarController.deleteCalendar);
+
+module.exports = router;
+s
