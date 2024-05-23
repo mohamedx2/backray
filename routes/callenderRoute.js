a94119b71008fa376e2a3f84237cbd138dc7cc1f
mohamedx2 <hamroun1969@gmail.com>
2024-05-23 15:51:31 +0100

    Update callenderRoute.js

--- routes/callenderRoute.js
+++ routes/callenderRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const calendarController = require('../controllers/callenderController');
-const authMiddleware = require('../middleware/verifyToken');
+const authMiddleware = require('../middlewares/verifyToken');
 
 const router = express.Router();
 
