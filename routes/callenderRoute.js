48080334160154a93581dee15fb56fcc2847cd01
mohamedx2 <hamroun1969@gmail.com>
2024-06-02 17:34:29 +0100

    Update callenderRoute.js

--- routes/callenderRoute.js
+++ routes/callenderRoute.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const calendarController = require('../controllers/callenderController'); // Adjust this path as necessary
-const {verifyAdminToken} = require('../middlewares/verifyToken'); // Adjust this path as necessary
+const {verifyAdminToken,verifyToken} = require('../middlewares/verifyToken'); // Adjust this path as necessary
 
 router.post('/not-working',verifyAdminToken, calendarController.getUsersNotWorkingInTimeRange);
 router.post('/',verifyAdminToken, calendarController.createCalendar);
-router.get('/user', calendarController.getCalendarsByUser);
+router.get('/user',verifyToken, calendarController.getCalendarsByUser);
 router.get('/',verifyAdminToken, calendarController.getAllCalendars);
 router.get('/:id',verifyAdminToken, calendarController.getCalendarById);
 router.patch('/:id',verifyAdminToken,  calendarController.updateCalendar); // Protect this route with verifyAdmin
